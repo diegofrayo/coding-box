aaadab5466d683fe85fff9f5be9735c0bb8cb27d
Diego Rayo <diegofrayo@gmail.com>
2022-12-16 16:23:40 -0500

    :fire: [delete] svelte project

---
+++ svelte-playground/src/components/Image.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let filter = '';
+
+	onMount(() => {
+		console.log('The "Image" component has mounted');
+	});
+</script>
+
+<img src="/photo.png" class={`image ${filter}`.trim()} alt="Joy the last creation" />
+
+<svg>
+	<filter id="custom-filter-wavy">
+		<feTurbulence id="turbulence" type="turbulence" numOctaves="1" result="NOISE" />
+		<feDisplacementMap in="SourceGraphic" in2="NOISE" scale="50" />
+		<animate
+			xlink:href="#turbulence"
+			attributeName="baseFrequency"
+			dur="60s"
+			keyTimes="0;0.5;1"
+			values="0.01 0.02;0.02 0.04;0.01 0.02"
+			repeatCount="indefinite"
+		/>
+	</filter>
+</svg>
+
+<style>
+	.image {
+		width: 300px;
+	}
+
+	.image.filter-1 {
+		filter: blur(5px);
+	}
+
+	.image.filter-2 {
+		filter: contrast(200%);
+	}
+
+	.image.filter-3 {
+		filter: grayscale(80%);
+	}
+
+	.image.filter-4 {
+		filter: hue-rotate(90deg);
+	}
+
+	.image.filter-5 {
+		filter: drop-shadow(16px 16px 20px red) invert(75%);
+	}
+
+	.image.filter-6 {
+		filter: saturate(200%);
+	}
+
+	.image.filter-7 {
+		filter: url(#custom-filter-wavy);
+	}
+</style>
---
+++ svelte-playground/src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Image from './../components/Image.svelte';
+
+	let filters = [
+		{ id: 'no-filter', name: 'no-filter' },
+		{ id: 'filter-1', name: 'blur' },
+		{ id: 'filter-2', name: 'contrast' },
+		{ id: 'filter-3', name: 'grayscale' },
+		{ id: 'filter-4', name: 'hue-rotate' },
+		{ id: 'filter-5', name: 'drop-shadow' },
+		{ id: 'filter-6', name: 'saturate' },
+		{ id: 'filter-7', name: 'custom-svg-filter' }
+	];
+	let selectedFilter = filters[0];
+
+	function handleClick(event) {
+		const filterId = event.target.getAttribute('data-filter-id');
+		selectedFilter = filters.find((filter) => filter.id === filterId);
+	}
+</script>
+
+<div class="container">
+	<Image filter={selectedFilter.id} />
+	<p>{selectedFilter.name}</p>
+	<div>
+		{#each filters as filter}
+			<button class="filter-btn" on:click={handleClick} data-filter-id={filter.id}>
+				{filter.name}</button
+			>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.container {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		justify-content: center;
+		width: 100%;
+	}
+
+	.filter-btn {
+		margin: 10px;
+	}
+</style>
